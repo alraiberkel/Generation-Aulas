b18fd3bfa51e500d928305f6d72ec600319c892f
Ela Oliveira <89111102+alraiberkel@users.noreply.github.com>
2021-09-13 18:12:02 -0300

    Update Estoque.java

--- collection/Estoque.java
+++ collection/Estoque.java
@@ -9,22 +9,22 @@ public class Estoque {
 
         
 	String dpt1 = "Quant de camisetas: 500";
-    String dpt2 = "Quant de calças: 400";
-    String dpt3 = "Quant de shots: 300";
-    String dpt4 = "Quant de meia-calça: 250";
+    	String dpt2 = "Quant de calÃ§as: 400";
+    	String dpt3 = "Quant de shots: 300";
+    	String dpt4 = "Quant de meia-calÃ§a: 250";
 
-    ArrayList<String> Estoque = new ArrayList<>();
-    Estoque.add(dpt1);
-    Estoque.add(dpt2);
-    Estoque.add(dpt3);
-    Estoque.add(dpt4);
+    	ArrayList<String> Estoque = new ArrayList<>();
+    	Estoque.add(dpt1);
+    	Estoque.add(dpt2);
+    	Estoque.add(dpt3);
+    	Estoque.add(dpt4);
 
-    System.out.println("O estoque da loja" +Estoque);
+    	System.out.println("O estoque da loja" +Estoque);
     
     
-    System.out.println("Estoque");
-    Estoque.remove(2);
-    System.out.println("O estoque da loja foi atualizado" + Estoque); 
+    	System.out.println("Estoque");
+    	Estoque.remove(2);
+    	System.out.println("O estoque da loja foi atualizado" + Estoque); 
     
 	}
 }
